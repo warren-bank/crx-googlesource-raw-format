18ca8f2d11d0a1bc9e5e56de0611bde87c8ee422
Warren R Bank <warren.r.bank@gmail.com>
2019-09-17 02:04:26 -0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,27 @@
+### [GoogleSource Raw Format](https://github.com/warren-bank/crx-googlesource-raw-format)
+
+Chrome extension for "GoogleSource" website to enable downloading files from repos in raw format
+
+#### UI:
+
+* adds a link at the bottom-right corner of the page
+  * appears to the right of the already available format options: "text", "json"
+  * new format option: "raw"
+* when this link is clicked:
+  * the file contents are fetched in base64 format
+  * the data is decoded
+  * a download dialog appears
+  * you can choose where to save the file
+
+#### Examples:
+
+* "aapt.exe" v0.2-4509860
+  * [Windows](https://android.googlesource.com/platform/prebuilts/sdk/+/platform-tools-29.0.3/tools/windows/bin/aapt.exe)
+  * [Linux](https://android.googlesource.com/platform/prebuilts/sdk/+/platform-tools-29.0.3/tools/linux/bin/aapt)
+* "apksigner.jar" v27.0.3
+  * [cross-platform](https://chromium.googlesource.com/android_tools/+/c22a664c39af72dd8f89200220713dcad811300a/sdk/build-tools/27.0.3/lib/apksigner.jar)
+
+#### Legal:
+
+* copyright: [Warren Bank](https://github.com/warren-bank)
+* license: [GPL-2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
---
+++ crx/content.js
@@ -0,0 +1,120 @@
+var payload = function(){
+  const enable_debugger = true
+
+  const download_raw_format = function(event) {
+    if (enable_debugger) debugger;
+
+    event.preventDefault()
+    event.stopPropagation()
+
+    const url = window.location.protocol + '//' + window.location.hostname + window.location.pathname + '?format=TEXT'
+
+    fetch(url)
+    .then(response => {
+      if (
+           (!response.ok)
+        || (!response.status === 200)
+      ) {
+        throw 'error'
+      }
+
+      return response.text()
+    })
+    .then(base64 => {
+      if (!base64) {
+        throw 'error'
+      }
+
+      if (enable_debugger) console.log(base64)
+
+      // decode base64
+      return atob(base64)
+    })
+    .then(byteString => {
+      if (!byteString) {
+        throw 'error'
+      }
+
+      const ab = new ArrayBuffer(byteString.length)
+      const ia = new Uint8Array(ab)
+
+      for (let i=0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i)
+      }
+
+      // convert to blob
+      const blob = new Blob([ab], {type: 'application/octet-stream'})
+
+      // object URL
+      return URL.createObjectURL(blob)
+    })
+    .then(obj_url => {
+      if (!obj_url) {
+        throw 'error'
+      }
+
+      const name = window.location.pathname.replace(/^.*\/([^\/]+)$/, '$1')
+
+      // construct an in-memory download link
+      const anchor = document.createElement('a')
+      anchor.setAttribute('download', name)
+      anchor.setAttribute('href',     obj_url)
+
+      // trigger download dialog
+      anchor.click()
+    })
+    .catch(error => {})
+  }
+
+  const inject_link = function() {
+    if (enable_debugger) debugger;
+
+    const links_container = document.querySelector('body > footer > div.Footer > span.Footer-formats')
+
+    // sanity check
+    if (!links_container) return
+
+    const existing_link = links_container.querySelector('a')
+    if (!existing_link) return
+
+    const new_link = existing_link.cloneNode()
+    new_link.style.marginLeft = '20px'
+    new_link.innerText = 'raw'
+    new_link.setAttribute('href', '?format=RAW')
+    new_link.addEventListener('click', download_raw_format, false)
+
+    links_container.appendChild(new_link)
+  }
+
+  inject_link()
+}
+
+var get_hash_code = function(str){
+  var hash, i, char
+  hash = 0
+  if (str.length == 0) {
+    return hash
+  }
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i)
+    hash = ((hash<<5)-hash)+char
+    hash = hash & hash  // Convert to 32bit integer
+  }
+  return Math.abs(hash)
+}
+
+var inject_function = function(_function){
+  var inline, script, head
+
+  inline = _function.toString()
+  inline = '(' + inline + ')()' + '; //# sourceURL=crx_extension.' + get_hash_code(inline)
+  inline = document.createTextNode(inline)
+
+  script = document.createElement('script')
+  script.appendChild(inline)
+
+  head = document.head
+  head.appendChild(script)
+}
+
+inject_function(payload)
