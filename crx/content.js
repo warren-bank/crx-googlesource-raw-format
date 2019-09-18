3dc1a09c945b7781bba5f3865c508db414a53bc5
Warren R Bank <warren.r.bank@gmail.com>
2019-09-18 03:24:35 -0700

    v2.1.1: minor refactoring

--- crx/content.js
+++ crx/content.js
@@ -99,10 +99,13 @@ var payload = function(){
       && (document.body.innerText.trim() === '400: Bad Request')
     ) {
       document.body.innerHTML = '<h3>Downloading&hellip;</h3>'
+      document.title = 'raw download'
+
       download_raw_format()
       .then((name) => {
         document.body.innerHTML = '<h3>Download complete!</h3>'
-        document.title = name || 'raw download'
+        if (name)
+          document.title = name
       })
     }
     else {
